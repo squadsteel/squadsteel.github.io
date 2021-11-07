1cf4d8146cd64415f8e3c21b31dff0b1c3ac03e7
fahad <fahadsattar8@gmail.com>
2021-11-07 10:59:34 -0800

    ps

--- test/diyps2021.js
+++ test/diyps2021.js
@@ -1,44 +1,62 @@
 var img;
-var initials ='jm'; // your initials
+var initials ='fs'; // your initials
 var choice = '1'; // starting choice, so it is not empty
-var screenbg = 250; // off white background
-var lastscreenshot=61; // last screenshot never taken
-
+var screenbg = 240; // off white background
+var click = 1;
 function preload() {
 // preload() runs once, it may make you wait
-  img = loadImage('cat.jpg');  // cat.jpg needs to be next to this .js file
+  img = loadImage('74.png');  // cat.jpg needs to be next to this .js file
 // you can link to an image on your github account
-  //img = loadImage('https://dma-git.github.io/images/74.png');
+// img = loadImage('https://dma-git.github.io/images/74.png');
 }
 
 function setup() {
 createCanvas(600, 400);  // canvas size
 background(screenbg);   // use our background screen color
-
 }
 
 function draw() {
-    fill(225,0,0);
-  rect(0,0,100,30);  
-      fill(225,225,0);
-  rect(200,0,100,30);
-      fill(225,140,0);    
-  rect(100,0,100,30);
-      fill(0,150,0);
-  rect(300,0,100,30);
-      fill(0,0,225);
-  rect(400,0,100,30);
-      fill(140,0,140);
-  rect(500,0,100,30);
+ 
 
 
-  if (keyIsPressed) {
-    choice = key; // set choice to the key that was pressed
-    clear_print(); // check to see if it is clear screen or save image
+
+  click++;
+  if ( click ==1 ){
+  stroke(225,0,0);//red
   }
+      if ( click == 2 ){
+    stroke(225,140,0);//orange 
+      }
+       if (click ==3 ){
+        stroke(225,225,0);//yellow
+       }
+           if ( click ==4 ){
+            stroke(0,150,0);//green
+           }
+                 if( click ==5 ){
+                  stroke(0,0,225);//blue 
+                 }
+                  if ( click ==6 ){
+                  stroke(140,0,140);//purple 
+                  }
+                  
+         if (click == 6){
+           click =0;
+         }
+         strokeWeight(8);
+            // line(mouseX, mouseY, pmouseX, pmouseY);
+
   if (mouseIsPressed){
-    newkeyChoice(choice);  // if the mouse is pressed call newkeyChoice
-  }
+                line(mouseX, mouseY, pmouseX, pmouseY);
+
+}
+
+if (keyIsPressed === true) {
+      image(img, mouseX-75, mouseY-25, 75, 50);
+
+  } else {
+
+}
 }
 
 function newkeyChoice(toolChoice) { //toolchoice is the key that was pressed
@@ -48,30 +66,33 @@ function newkeyChoice(toolChoice) { //toolchoice is the key that was pressed
 
  if (toolChoice == '1' ) {  // first tool
    
-    stroke(225,0,0);
-    line(mouseX, mouseY, pmouseX, pmouseY);
+    stroke(01);
+   
+    ellipse(mouseX, mouseY, 10, 10);
+     line(mouseX, mouseY, pmouseX, pmouseY);
     
   } else if (toolChoice == '2') { // second tool
-   stroke(225,225,0);
 
- 
+    stroke(20);
     line(mouseX, mouseY, pmouseX, pmouseY);
   } else if (toolChoice == '3') { // third tool
 
-      stroke(225,140,0);    
+    stroke(300, 100, 0, 80);
     line(mouseX, mouseY, pmouseX, pmouseY);
   } else if (toolChoice == '4') {
-      stroke(0,150,0);
 
+    stroke(0, 0, 255);
     line(mouseX, mouseY, pmouseX, pmouseY);
   } else if (key == '5') { // this tool calls a function
-      stroke(0,0,225);
-
+    stroke(0, 0, 255);
+    testbox(20, 20, 200);
+    testbox(200, 20, 20);
     
-    line(mouseX, mouseY, pmouseX, pmouseY);
+ // make testbox do something!
+ //   line(mouseX, mouseY, pmouseX, pmouseY);
   } else if (toolChoice == '6') {
-      stroke(140,0,140);
 
+    stroke(200);
     line(mouseX, mouseY, pmouseX, pmouseY);
   } else if (toolChoice == '7') {
 
@@ -90,7 +111,7 @@ function newkeyChoice(toolChoice) { //toolchoice is the key that was pressed
     fill(random(255), random(255), random(255), random(255));
     rect(mouseX, mouseY, 200, 150);
   } else if (toolChoice == 'g' || toolChoice == 'G') { // g places the image we pre-loaded
-    image(img, mouseX, mouseY, 50, 50);
+    image(img, mouseX-25, mouseY-25, 50, 50);
     
   }
  }
@@ -111,16 +132,14 @@ function clear_print() {
     background(screenbg); // set the screen back to the background color
   } else if (key == 'p' || key == 'P') {
      saveme();  // call saveme which saves an image of the screen
+     key = ""; // reset the key so that it doesn't keep taking screenshots
   }
 }
 
 function saveme(){
     //this will save the name as the intials, date, time and a millis counting number.
     // it will always be larger in value then the last one.
-  filename=initials+day() + hour() + minute() +second();
-  if (second()!=lastscreenshot) { // don't take a screenshot if you just took one
+    filename=initials+day() + hour() + minute() +second();
     saveCanvas(filename, 'jpg');
-  }
-  lastscreenshot=second(); // set this to the current second so no more than one per second
-  
+ // }
 }
